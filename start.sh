fad597ae8d4b028e24dd0e205ef8cfcda9c2a322
kabagemark <kabagemark@gmail.com>
2018-09-15 14:38:58 +0300

    user class added to models

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 
 export DATABASE_URL='postgres://ahinwhmcidfiqc:7d42396d8a873d6a9ffa760971ac93dd94d8dbf771b9649e2040f4a8f4b00cc2@ec2-54-83-29-34.compute-1.amazonaws.com:5432/db0bj21elia979'
-
-
+export SECRET_KEY='kabagemark'
 
 python3.6 manage.py server
