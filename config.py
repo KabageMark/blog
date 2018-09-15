829c20d2c13730aafd510893a690b71ecf227a91
kabagemark <kabagemark@gmail.com>
2018-09-15 11:48:17 +0300

    config and serer setup

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://markchege:newpassword@localhost/oneminutepitch2'
+
+    
+
+    
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_PURPLE_URL)
+    pass
+    
+
+
+class DevConfig(Config):
+    DEBUG = True
+ 
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
